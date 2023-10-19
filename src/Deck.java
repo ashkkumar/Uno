855ef35c314bc160b11175720852c1e48970c55f
ashkumar <ashkuma5@gmail.com>
2023-10-19 19:51:56 -0400

    Deck Class

---
+++ src/Deck.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Deck {
+
+    public ArrayList<Card> deck;
+
+    public Deck(){
+        deck = new ArrayList<Card>();
+    }
+
+    public makeCards(int n, ){
+
+        for (int i = 0;;i++){
+            for (int j = 0;;++){
+                Card card = new Card(Card.cardType[i], )
+            }
+        }
+    }
+}
