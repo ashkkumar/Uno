4c5ce01213e50152f142b751542d572b5969ca79
tommycsete <tommycsete@gmail.com>
2023-10-20 15:31:12 -0400

    Created Tests for Card

--- src/CardTest.java
+++ src/CardTest.java
@@ -2,34 +2,55 @@
  * Card Class Testing
  */
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CardTest {
     private static Card card;
+    private static int counter;
 
     public CardTest(){}
 
+    /**
+     * Sets test card and counter to default values before each test
+     */
     @BeforeEach
     public void setUp(){
         card = null;
+        counter = 0;
     }
 
+    /**
+     * Displays the counter value for each test after they are complete
+     */
+    @AfterEach
+    public void summary(){
+        System.out.println("Number of Tests Completed: " + counter + "\n");
+    }
     @Test
     public void test_OverloadedConstructor(){
-
+        System.out.println("Testing Overloaded Constructor...");
+        card = new Card(Card.CardType.SIX, Card.Colour.BLUE);
+        assertEquals(Card.CardType.SIX, card.getCardType());
+        assertEquals(Card.Colour.BLUE, card.getColour());
+        counter = 2;
     }
 
     @Test
     public void test_getCardType(){
-
+        System.out.println("Testing getCardType...");
+        card = new Card(Card.CardType.DRAWONE, Card.Colour.RED);
+        assertEquals(Card.CardType.DRAWONE, card.getCardType());
+        counter = 1;
     }
 
     @Test
     public void test_getColour(){
-
+        System.out.println("Testing getColour...");
+        card = new Card(Card.CardType.REVERSE, Card.Colour.YELLOW);
+        assertEquals(Card.Colour.YELLOW, card.getColour());
+        counter = 1;
     }
-
-
 }
