10beab789483c3eb5d70abb640ed365a4768638a
tommycsete <tommycsete@gmail.com>
2023-11-13 23:39:15 -0500

    Small format changes

--- src/UnoViewHandler.java
+++ src/UnoViewHandler.java
@@ -3,6 +3,5 @@ public interface UnoViewHandler {
     void handleNextTurn(UnoGameEvent e);
     void handleDrawCard(UnoGameEvent e);
     void handlePlay(UnoGameEvent e);
-
     void handleUnoCall(UnoGameEvent e);
 }
