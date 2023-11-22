9463d925adb70d2bba3ca0ee45ec8ec7a5fa6d88
ashkumar <ashkuma5@gmail.com>
2023-11-22 15:52:09 -0500

    changes

--- src/Player.java
+++ src/Player.java
@@ -2,49 +2,148 @@ import java.util.ArrayList;
 
 public class Player {
 
-    String name;
+    private String name;
 
-    ArrayList<Card> myCards;
+    private ArrayList<Card> myCards;
 
-    int score;
+    private int score;
 
-    int numCards;
+    private int numCards;
 
-    public Player(){
+    private boolean canPlay = true;
+
+    private boolean hasDrawn = false;
+
+    /**
+     * Constructs a new Uno player with an empty card list and a score of 0.
+     */
+    public Player() {
+
         myCards = new ArrayList<Card>();
         numCards = 0;
     }
 
-    public void addCard(Card card){
-        myCards.add(card);
-        numCards++;
-    }
-
-    public void removeCard(Card card){
-        myCards.remove(card);
-        numCards--;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
+    /**
+     * Gets the player's score.
+     *
+     * @return The player's score.
+     */
     public int getScore() {
         return score;
     }
 
+    public boolean canPlay(){
+        return canPlay;
+    }
+
+    /**
+     * Gets the number of cards in the player's hand.
+     *
+     * @return The count of cards in the player's hand.
+     */
     public int getNumCards() {
         return numCards;
     }
 
-    public void getMyCards() {
+    public boolean getHasDrawn(){
+        return hasDrawn;
+    }
+
+    /**
+     * Gets the list of cards in the player's hand.
+     *
+     * @return An ArrayList containing the player's cards.
+     */
+    public ArrayList<Card> getMyCards() {
+        return myCards;
+    }
+
+    /**
+     * Gets the name of the player.
+     *
+     * @return The name of the player.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the player's score.
+     *
+     * @param score The score to set for the player.
+     */
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    /**
+     * Sets the name of the player.
+     *
+     * @param name The name to set for the player.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCanPlay(Boolean bool){
+        this.canPlay = bool;
+    }
+
+    public void setHasDrawn(boolean hasDrawn) {
+        this.hasDrawn = hasDrawn;
+    }
+
+    /**
+     * Adds a card to the player's hand.
+     *
+     * @param card The Uno card to add to the player's hand.
+     */
+    public void addCard(Card card) {
+        myCards.add(card);
+        numCards++;
+    }
+
+    /**
+     * Draws a card from the deck and adds it to the player's hand.
+     *
+     * @param deck The Uno deck from which to draw a card.
+     */
+    public Card drawCard(Deck deck) {
+        Card drawnCard = deck.draw();
+        myCards.add(drawnCard);
+        numCards++;
+        return drawnCard;
+    }
+
+    /**
+     * Removes a card from the player's hand by its index.
+     *
+     * @param n The index of the card to remove from the player's hand.
+     */
+    public void removeCard(Card card) {
+        myCards.remove(card);
+        numCards--;
+    }
+
+    /**
+     * Plays a card from the player's hand.
+     *
+     * @param index The index of the card to play in the player's hand.
+     * @return The Uno card played.
+     */
+    public Card playCard(int index) {
+        return myCards.get(index);
+    }
+
+    /**
+     * Displays the player's cards.
+     */
+    public void displayCards() {
         System.out.println("Your Cards:");
-        for (int i = 0; i < numCards; i++){
-            System.out.println(i + "." + myCards.get(i));
+        for (int i = 0; i < myCards.size(); i++) {
+            System.out.println(i + 1 + "." + myCards.get(i).toString());
         }
     }
 
-    public void drawCard(){
-
-    }
 }
+
